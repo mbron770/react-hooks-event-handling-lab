8ea9c93d8dd239d2d9899e92ddb221612c79fd63
mbron770 <mbronfin.tsd@gmail.com>
2023-06-14 01:51:47 -0400

    add JSX event listeners

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React, { useState } from "react"
+
+function EyesOnMe(){
+
+    function focusAction(){
+        console.log('Good!')
+    }
+
+    function blurAction(){
+        console.log('Hey! Eyes on me!')
+
+    }
+
+    return(
+        <button onFocus={focusAction} onBlur={blurAction}>Eyes on me</button>
+
+
+    )
+}
+
+export default EyesOnMe 
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,14 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function changeFunc(){
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
-    )
+        <input type = 'password' onChange = {changeFunc}/>
+   
+   
+        )
 }
 
 export default Keypad;
